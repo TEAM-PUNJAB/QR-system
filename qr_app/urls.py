15836e84162182f0d957149767cab484bd8ac1e5
Amanpree <84691091+Amanpree@users.noreply.github.com>
2021-05-28 19:26:59 +1000

    Add files via upload

---
+++ qr_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import login, dashboard, logout, set_ip, scan_page, check_in, check_out, download_csv
+
+urlpatterns = [
+    path('', login, name="login"),
+    path('dashboard/', dashboard, name="dash"),
+    path('logout/', logout, name="logout"),
+    path('set_ip/', set_ip, name="set_ip"),
+    path('scan_page/<slug:id>/', scan_page, name="scan_page"),
+    path('check_in/<slug:id>/', check_in, name="check_in"),
+    path('check_out/<slug:id>/', check_out, name="check_out"),
+    path('download_csv/', download_csv, name="download_csv"),
+]
